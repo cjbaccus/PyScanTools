d1ad02fc06178dd9fb5468437a791050f66366f7
Carl Baccus <cjbaccus@gmail.com>
2020-12-06 16:21:54 -0800

    split details and added **30 to each line

--- PyHost.py
+++ PyHost.py
@@ -7,7 +7,10 @@ net = IPv4Network("192.168.3.0/24")
 
 for addr in net:
 	try:
-		print(socket.gethostbyaddr(str(addr)))
+		print(socket.gethostbyaddr(str(addr))[0])
+		print(socket.gethostbyaddr(str(addr))[2])
+		print(" ")
+		print("*"*30)
 	except:
 		pass
 
